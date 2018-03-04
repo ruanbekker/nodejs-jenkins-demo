04e8460fd01ef574f002edbb3cfea9d03a111e8a
Ruan Bekker <ruan.ru.bekker@gmail.com>
2018-03-04 00:08:29 +0000

    fix expected text

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello, Jenkins!');
+  res.send('Hello, World');
 });
 
 app.listen(process.env.PORT || 5000);
